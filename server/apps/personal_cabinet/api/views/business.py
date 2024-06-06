49f83282e3a0e2285ea2f1a3d72392b1a63ec3ae
GlebMazur <mag@letsnova.ru>
2024-06-07 01:56:51 +0300

    fix: правка api create-business-by-inn

--- server/apps/personal_cabinet/api/views/business.py
+++ server/apps/personal_cabinet/api/views/business.py
@@ -121,22 +121,22 @@ class BusinessViewSet(RetrieveListCreateUpdateDeleteViewSet):
         """Создание бизнеса по ИНН."""
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)  # noqa: WPS204
-        # try:
-        update_or_create_business(
-            inn=serializer.validated_data['inn'],
-            user_id=request.user.id,
-        )
-        # except Exception:
-        #     return Response(
-        #         data={
-        #             'detail': _(
-        #                 'При добавлении по ИНН вашего бизнеса произошла '
-        #                 'ошибка, связанная с доступом к сервису DaData.'
-        #                 'Вы можете добавить информацию в ручную.'
-        #             ),
-        #         },
-        #         status=status.HTTP_400_BAD_REQUEST,
-        #     )
+        try:
+            update_or_create_business(
+                inn=serializer.validated_data['inn'],
+                user_id=request.user.id,
+            )
+        except Exception:
+            return Response(
+                data={
+                    'detail': _(
+                        'При добавлении по ИНН вашего бизнеса произошла '
+                        'ошибка, связанная с доступом к сервису DaData.'
+                        'Вы можете добавить информацию в ручную.'
+                    ),
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         return Response(
             data={'detail': _('Информация о бизнесе успешно добавлена')},
             status=status.HTTP_200_OK,

011b5c3f44909794ce2e6be69f5f2d3013e6d2e6
Mazur Gleb <mag@letsnova.ru>
2024-06-09 01:25:06 +0300

    fix: правка фильтров по проблеме

--- server/apps/support/api/views/problem_category.py
+++ server/apps/support/api/views/problem_category.py
@@ -4,6 +4,7 @@ from server.apps.services.filters_mixins import CreatedUpdatedDateFilterMixin
 from server.apps.services.views import BaseReadOnlyViewSet
 from server.apps.support.api.serializers import ProblemCategorySerializer
 from server.apps.support.models import ProblemCategory
+from django.utils.translation import gettext_lazy as _
 
 
 class ProblemCategoryFilter(
@@ -12,11 +13,42 @@ class ProblemCategoryFilter(
 ):
     """Фильтр инфраструктуры."""
 
+    problem_subcategory_id = django_filters.AllValuesMultipleFilter(
+        field_name='problem_subcategories__name',
+        label=_('Фильтр по id подкатегории проблемы')
+    )
+    problem_subcategory_name = django_filters.AllValuesMultipleFilter(
+        field_name='problem_subcategories__name',
+        label=_('Фильтр по названиям подкатегории проблемы')
+    )
+    problem_theme_id = django_filters.AllValuesMultipleFilter(
+        field_name='problem_subcategories__problem_themes__id',
+        label=_('Фильтр по id темы проблемы')
+    )
+    problem_theme_name = django_filters.AllValuesMultipleFilter(
+        field_name='problem_subcategories__problem_themes__name',
+        label=_('Фильтр по названиям темы проблемы')
+    )
+    problem_id = django_filters.AllValuesMultipleFilter(
+        field_name='problem_subcategories__problem_themes__problems__id',
+        label=_('Фильтр по названию проблем')
+    )
+    problem_name = django_filters.AllValuesMultipleFilter(
+        field_name='problem_subcategories__problem_themes__problems__name',
+        label=_('Фильтр по id проблем')
+    )
+
     class Meta:
         model = ProblemCategory
         fields = (
             'id',
             'name',
+            'problem_subcategory_id',
+            'problem_subcategory_name',
+            'problem_theme_name',
+            'problem_theme_id',
+            'problem_name',
+            'problem_id',
         )
 
 

b571b4f63a4b87d3d4a8002d44b7743afdecd31d
Mazur Gleb <mag@letsnova.ru>
2024-06-08 03:23:33 +0300

    fix: правка апишек

--- server/apps/investment_object/permissions/__init__.py
+++ server/apps/investment_object/permissions/__init__.py
@@ -1,5 +1,21 @@
-from server.apps.investment_object.permissions import economic_activity
+from server.apps.investment_object.permissions import (
+    economic_activity,
+    infrastructure,
+    investment_object,
+    privilege,
+    ready_business,
+    restriction,
+    specialized_site,
+    tender_lot,
+)
 
 __all__ = [
-    'economic_activity'
+    'economic_activity',
+    'infrastructure',
+    'investment_object',
+    'privilege',
+    'ready_business',
+    'restriction',
+    'specialized_site',
+    'tender_lot',
 ]

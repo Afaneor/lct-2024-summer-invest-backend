39ea0213550124eff7ec9f874e2ea971d04f3bb1
npavlin <np@letsnova.ru>
2024-06-13 22:08:40 +0300

    fix: disable csrf middleware

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -72,7 +72,7 @@ MIDDLEWARE: Tuple[str, ...] = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',

c630e54b4cb22ec4e263f34be09f3cf9a3c25592
Mazur Gleb <mag@letsnova.ru>
2024-06-15 13:15:34 +0300

    fix: тесты

--- server/apps/investment_object/api/serializers/base_data.py
+++ server/apps/investment_object/api/serializers/base_data.py
@@ -17,13 +17,23 @@ from server.apps.investment_object.models import (
 class BaseTransactionFormSerializer(serializers.ModelSerializer):
     """Сериалайзер формы транзакции."""
 
+    transaction_form_type_label = serializers.SerializerMethodField()
+
     class Meta:
         model = TransactionForm
         fields = (
             'id',
             'name',
             'transaction_form_type',
+            'transaction_form_type_label',
         )
+
+    def get_transaction_form_type_label(
+        self,
+        transaction_form: TransactionForm,
+    ):
+        """Подпись к transaction_form_type."""
+        return transaction_form.get_transaction_form_type_display()
 
 
 class BaseEconomicActivitySerializer(serializers.ModelSerializer):
